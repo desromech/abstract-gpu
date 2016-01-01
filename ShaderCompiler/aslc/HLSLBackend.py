5443a34778acfa9ad78a063d06a930ce4bfa2ec4
Ronie Salgado <roniesalg@gmail.com>
2016-01-01 12:31:29 -0300

    The shader compiler was moved into another repository.

--- ShaderCompiler/aslc/HLSLBackend.py
+++
@@ -1,9 +0,0 @@
-import _winreg
-
-def findSdkPath():
-	with _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\Microsoft\Windows Kits\Installed Roots') as key:
-		_winreg.QueryValue(key, 'KitsRoot10')
-		return str(_winreg[0])
-	
-def findFXC():
-	return findSdkPath() + r'\bin\fxc.exe'
