8dbb7148f46732cccc87affdc08b292b3dbc97c5
Ronie Salgado <roniesalg@gmail.com>
2018-01-14 20:27:30 -0300

    Debugging vulkan on travis.

--- .travis_build.sh
+++ .travis_build.sh
@@ -3,11 +3,9 @@
 # Use the vulkan SDK
 source ./thirdparty/vulkan-sdk/setup-env.sh
 
-echo "G++ version"
-g++ --version
-
-#echo "G++ 6 version"
-#g++-6 --version
+echo "Vulkan deps"
+echo "ldd ${VULKAN_SDK}/lib/libvulkan.so"
+ldd ${VULKAN_SDK}/lib/libvulkan.so
 
 echo "Generating cmake project"
 mkdir build
