44cd91b340de9c598b0d78eb9f46352df51ef4c4
Ronie Salgado <roniesalg@gmail.com>
2018-01-14 19:37:21 -0300

    Oops.

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo "Travis install script"
 echo "TRAVIS_OS_NAME $TRAVIS_OS_NAME"
-if "$TRAVIS_OS_NAME" == "linux"; then
+
+if test "$TRAVIS_OS_NAME" = "linux"; then
     echo "Updating APT"
 
     sudo apt-get -qq update || exit 1
-    
+
     echo "Installing dependencies"
     sudo apt-get -qqy install gcc-6-mulitlib g++-6-mulitlib libvulkan1 libvulkan-dev libvulkan1:i386 libvulkan-dev:i386 || exit 1
 fi
