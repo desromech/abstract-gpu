d56d68d0ac850e52e05a8f06753355f754430687
Ronie Salgado <roniesalg@gmail.com>
2018-03-10 23:17:56 -0300

    Appveyor.yml changes [skip ci]

--- scripts/appveyor-build.bat
+++ scripts/appveyor-build.bat
@@ -1,5 +1,7 @@
 @echo off
 
+cd /d "%~dp0"
+
 echo Appveyor build invoked
 echo Appveyor platform %PLATFORM%
 build-variant-%PLATFORM%.bat
--- scripts/appveyor-deploy.bat
+++ scripts/appveyor-deploy.bat
@@ -1,3 +1,5 @@
 @echo off
 
+cd /d "%~dp0"
+
 echo TODO: Appveyor deploy artifacts
