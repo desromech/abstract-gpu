ba56a49172088001ff689fbd710135854e083cef
Ronie Salgado <roniesalg@gmail.com>
2019-10-12 01:43:31 -0300

    I fixed the compilation of agpu in OS X.

--- implementations/Metal/texture.hpp
+++ implementations/Metal/texture.hpp
@@ -18,6 +18,7 @@ public:
     virtual agpu_pointer mapLevel(agpu_int level, agpu_int arrayIndex, agpu_mapping_access flags, agpu_region3d *region) override;
     virtual agpu_error unmapLevel() override;
     virtual agpu_error readTextureData(agpu_int level, agpu_int arrayIndex, agpu_int pitch, agpu_int slicePitch, agpu_pointer buffer) override;
+    virtual agpu_error readTextureSubData(agpu_int level, agpu_int arrayIndex, agpu_int pitch, agpu_int slicePitch, agpu_region3d* sourceRegion, agpu_size3d* destSize, agpu_pointer buffer) override;
     virtual agpu_error uploadTextureData(agpu_int level, agpu_int arrayIndex, agpu_int pitch, agpu_int slicePitch, agpu_pointer data) override;
     virtual agpu_error uploadTextureSubData(agpu_int level, agpu_int arrayIndex, agpu_int pitch, agpu_int slicePitch, agpu_size3d* sourceSize, agpu_region3d* destRegion, agpu_pointer data) override;
     virtual agpu_error getFullViewDescription(agpu_texture_view_description* result) override;
