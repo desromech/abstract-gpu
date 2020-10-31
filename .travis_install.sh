7cca165d6ac547c19d72e7b3a1d8b9df3e8e68b8
Ronie Salgado <roniesalg@gmail.com>
2020-10-31 23:29:53 +0100

    I bumped up the Linux distro from Trusty to Bionic in Travis. I bumped up the included version of the vulkan sdk includes and libraries for building under Windows in AppVeyor.

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 echo "Travis install script"
 echo "TRAVIS_OS_NAME $TRAVIS_OS_NAME"
@@ -6,17 +7,8 @@ echo "TRAVIS_OS_NAME $TRAVIS_OS_NAME"
 if test "$TRAVIS_OS_NAME" = "linux"; then
     echo "Updating APT"
     #sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-get -qq update || exit 1
+    sudo apt-get -qq update
 
     echo "Installing 64 bits dependencies"
-    #sudo apt-get -qqy install gcc-6-multilib g++-6-multilib libx11-xcb-dev || exit 1
-    sudo apt-get -qqy install libx11-xcb-dev || exit 1
-
-    echo "Downloading the Vulkan SDK"
-    wget -Ovulkansdk-linux.run "http://ronie.cl/files/ci/vulkansdk-linux-x86_64-1.0.65.0.run" || exit 1
-
-    echo "Extracting the Vulkan SDK"
-    chmod +x ./vulkansdk-linux.run || exit 1
-    ./vulkansdk-linux.run || exit 1
-    mv -v VulkanSDK/1.0.65.0 thirdparty/vulkan-sdk || exit 1
+    sudo apt-get -qqy install libx11-xcb-dev libvulkan-dev
 fi
