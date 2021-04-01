19255148064b370753642de90cc811d34c2b0bb5
Ronie Salgado <roniesalg@gmail.com>
2021-03-31 23:00:40 -0300

    I added the scripts for building under GitHub Actions in Windows.

--- scripts/appveyor-build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set VULKAN_SDK=c:\projects\agpu\thirdparty\vulkan-sdk-windows
-
-cd /d "%~dp0"
-
-echo Appveyor build invoked
-echo Appveyor platform %PLATFORM%
-build-variant-%PLATFORM%.bat
